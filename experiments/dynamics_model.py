7e8080feafc76cf3e72de06de514e0cf547a3deb
nicolasbougie <nicolasbougie@gitlab.localhost>
2022-10-13 17:44:55 +0900

    Added dynamics model

--- experiments/dynamics_model.py
+++ experiments/dynamics_model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from torch import optim
 
 
 class Transpose(nn.Module):
@@ -54,12 +55,55 @@ class DynamicsModel(nn.Module):
                 nn.init.constant_(module.bias, 0)
 
     def forward(self, state, next_state, action):
-        state_ft = self.conv(state)
-        next_state_ft = self.conv(next_state)
+        state_ft = self.encoder(state)
+        next_state_ft = self.encoder(next_state)
         state_ft = state_ft.view(-1, self.feature_size)
         next_state_ft = next_state_ft.view(-1, self.feature_size)
         return self.inverse_net(torch.cat((state_ft, next_state_ft), 1)), self.forward_net(
             torch.cat((state_ft, action), 1)), next_state_ft
+
     
     
-m = DynamicsModel((16, 16, 27), (334,))
+def train(device, nb_epochs = 5):
+    #not finished
+    inv_criterion = nn.CrossEntropyLoss()
+    fwd_criterion = nn.MSELoss()
+    m = DynamicsModel((16, 16, 27), (334,))
+    m = m.to(device)
+    optimizer = optim.Adam(m.parameters(), lr=1e-4, eps=1e-5)
+    
+    inv_losses = []
+    fwd_losses = []
+    total_losses = []
+    
+    beta = 0.2
+    
+    for _ in range(nb_epochs):
+        state, action, next_state = torch.rand((32,16, 16, 27)), torch.rand((32,334)), torch.rand((32,16, 16, 27))
+        
+        pred_logits, pred_phi, phi = m(state, next_state, action)
+        action_oh = torch.zeros((32),dtype=torch.long)
+        
+        inv_loss = inv_criterion(pred_logits, action_oh)
+        fwd_loss = fwd_criterion(pred_phi, phi) / 2
+        
+        
+        total_loss = (1 - beta) * inv_loss + beta * fwd_loss
+        optimizer.zero_grad()
+        total_loss.backward()
+        
+        inv_losses.append(inv_loss)
+        fwd_losses.append(fwd_loss)
+        total_losses.append(total_loss)
+        print("Total loss {}".format(total_loss.detach().item()))
+        
+        
+        
+        
+    
+    
+    
+    
+    
+device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+train(device , nb_epochs=5)
